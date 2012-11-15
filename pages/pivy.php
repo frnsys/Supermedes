b5bd3c62fa9d652cb9a65fa1d6f286e123c227c1
Francis Tseng <ftzeng@gmail.com>
2012-11-14 21:31:35 -0800

    added pivy and integrated svg

---
+++ pages/pivy.php
@@ -0,0 +1,29 @@
+<?php include 'header.html' ?>
+
+<div class="page" id="pivy">
+	<div class="detail">
+		<div class="info">
+			<div class="icon"></div>
+			<div class="text">
+				<h2>Pivy</h2>
+				<p>A visualizer for monitoring team progress and capacity with Pivotal Tracker.</p>
+			</div>
+		</div>
+		<div class="stage">
+			<figure>
+				<img src="/img/portfolio/shots/pivy.png" alt="Pivy" />
+			</figure>
+			<figure>
+				<img src="/img/portfolio/shots/pivy2.png" alt="Pivy" />
+			</figure>
+			<figure>
+				<img src="/img/portfolio/shots/pivy3.png" alt="Pivy" />
+			</figure>
+			<figure>
+				<img src="/img/portfolio/shots/pivy4.png" alt="Pivy" />
+			</figure>
+		</div>
+	</div>
+</div>
+
+<?php include 'footer.html' ?>
