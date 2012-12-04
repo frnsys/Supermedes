06255be320b32f87f33be2ebf69a1a4c75e077e4
Francis Tseng <ftzeng@gmail.com>
2012-12-03 22:24:05 -0800

    added bicephaly, fixed svg offsets

---
+++ pages/bicephaly.php
@@ -0,0 +1,30 @@
+<?php include 'header.html' ?>
+
+<div class="page" id="bicephaly">
+	<div class="detail">
+		<div class="info">
+			<div class="icon"></div>
+			<div class="text">
+				<h2>Bicephaly Pictures</h2>
+				<p>A completely responsive and rich portfolio for a NY-based production company.</p>
+				<a href="http://www.bicephalypictures.com">Visit Bicephaly Pictures</a>
+			</div>
+		</div>
+		<div class="stage">
+			<figure>
+				<img src="/img/portfolio/shots/bicephaly1.jpg" alt="Bicephaly Pictures" />
+			</figure>
+			<figure>
+				<img src="/img/portfolio/shots/bicephaly2.jpg" alt="Bicephaly Pictures" />
+			</figure>
+			<figure>
+				<img src="/img/portfolio/shots/bicephaly3.jpg" alt="Bicephaly Pictures" />
+			</figure>
+			<figure>
+				<img src="/img/portfolio/shots/bicephaly4.jpg" alt="Bicephaly Pictures" />
+			</figure>
+		</div>
+	</div>
+</div>
+
+<?php include 'footer.html' ?>
