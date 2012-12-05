31643e8fe0abedf027197a19ae14835d6bbd508f
Francis Tseng <ftzeng@gmail.com>
2012-12-04 19:31:24 -0800

    added wikidigest

---
+++ pages/wikidigest.php
@@ -0,0 +1,26 @@
+<?php include 'header.html' ?>
+
+<div class="page" id="wikidigest">
+	<div class="detail">
+		<div class="info">
+			<div class="icon"></div>
+			<div class="text">
+				<h2>Wikidigest</h2>
+				<p>A Wikipedia database dump parser & digester.</p>
+				<p>
+					Wikidigest is meant to import a Wikipedia XML dump into a MySQL database.	
+					It's written in Ruby which means it's fairly slow; I hope to later 
+					rewrite it in a much faster language.
+				</p>
+				<a href="https://github.com/ftzeng/wikidigest">Visit Wikidigest on Github</a>
+			</div>
+		</div>
+		<div class="stage">
+			<figure>
+				<img src="/img/portfolio/shots/wikidigest.png" alt="wikidigest" />
+			</figure>
+		</div>
+	</div>
+</div>
+
+<?php include 'footer.html' ?>
