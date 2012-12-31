9becd3357596ef5aa4d768d43acfc99da3ffb940
Francis Tseng <ftzeng@gmail.com>
2012-12-31 00:49:44 -0500

    updated link to blog

--- pages/wikidigest.php
+++ pages/wikidigest.php
@@ -8,9 +8,12 @@
 				<h2>Wikidigest</h2>
 				<p>A Wikipedia database dump parser & digester.</p>
 				<p>
-					Wikidigest is meant to import a Wikipedia XML dump into a MySQL database.	
-					It's written in Ruby which means it's fairly slow; I hope to later 
-					rewrite it in a much faster language.
+					Wikidigest will "digest" a Wikipedia XML data dump into a graph for natural language processing – 
+					each article is sanitized & stemmed, 
+					then, through TF*IDF, saved as descriptive word vectors into a MySQL DB.</p>
+				<p>
+					It's written in Ruby which means it's fairly slow (not to mention Wikipedia dumps are <em>huge</em>). I hope to later 
+					rewrite it in a faster language with a proper graph db.
 				</p>
 				<a href="https://github.com/ftzeng/wikidigest">Visit Wikidigest on Github</a>
 			</div>
