5aa16758e7916d398332c6f88dcc2616690410ad
Francis Tseng <ftzeng@gmail.com>
2013-04-18 22:51:41 -0400

    updated cv stuff, updated nito

--- pages/nito.php
+++ pages/nito.php
@@ -7,6 +7,7 @@
 			<div class="text">
 				<h2>NITO</h2>
 				<p>A steganographic tool for encoding and decoding sensitive information into images using "context keys".</p>
+				<p>Caveat: NITO was written before I really knew much about steganography. Compared to what's available, it's a relatively simple and easy-to-crack implementation. So be warned!</p>
 				<a href="/projects/nito">Visit project page</a>
 				<a href="/files/nito.zip" class="download">Download NITO</a>
 			</div>
