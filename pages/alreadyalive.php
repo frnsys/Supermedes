457b841d388e5c16a41bd9ed67d9c31a02cccf1a
Francis Tseng <ftzeng@gmail.com>
2013-06-15 22:31:02 -0400

    updating projects

---
+++ pages/alreadyalive.php
@@ -0,0 +1,33 @@
+<?php include 'header.html' ?>
+
+<div class="page" id="alreadyalive">
+	<div class="detail">
+		<div class="info">
+			<div class="icon"></div>
+			<div class="text">
+				<h2>Already Alive</h2>
+                <p>A responsive and polished portfolio for the Already Alive production company.</p>
+				<a href="http://www.alreadyalive.com">Visit Already Alive</a>
+			</div>
+		</div>
+		<div class="stage">
+			<figure>
+				<img src="/img/portfolio/shots/alreadyalive1.jpg" alt="Already Alive" />
+			</figure>
+			<figure>
+				<img src="/img/portfolio/shots/alreadyalive2.jpg" alt="Already Alive" />
+			</figure>
+			<figure>
+				<img src="/img/portfolio/shots/alreadyalive3.jpg" alt="Already Alive" />
+			</figure>
+			<figure>
+				<img src="/img/portfolio/shots/alreadyalive4.jpg" alt="Already Alive" />
+			</figure>
+			<figure>
+				<img src="/img/portfolio/shots/alreadyalive5.jpg" alt="Already Alive" />
+			</figure>
+		</div>
+	</div>
+</div>
+
+<?php include 'footer.html' ?>
---
+++ pages/futora.php
@@ -0,0 +1,26 @@
+<?php include 'header.html' ?>
+
+<div class="page" id="futora">
+	<div class="detail">
+		<div class="info">
+			<div class="icon"></div>
+			<div class="text">
+				<h2>Futora</h2>
+                <p>A colorscheme for Vim.</p>
+				<a href="https://github.com/ftzeng/futora/">Visit Futora on Github</a>
+			</div>
+		</div>
+		<div class="stage">
+			<figure>
+				<img src="/img/portfolio/shots/futora1.png" alt="Futora" />
+                <figcaption>Javascript</figcaption>
+			</figure>
+			<figure>
+				<img src="/img/portfolio/shots/futora2.png" alt="Futora" />
+                <figcaption>Python</figcaption>
+			</figure>
+		</div>
+	</div>
+</div>
+
+<?php include 'footer.html' ?>
