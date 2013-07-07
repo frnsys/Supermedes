06acbb103d54e4240963da3091ca61b249989a3a
Francis Tseng <ftzeng@gmail.com>
2013-07-07 18:38:05 -0400

    added tor

---
+++ pages/tor.php
@@ -0,0 +1,26 @@
+<?php include 'header.html' ?>
+
+<div class="page" id="tor">
+	<div class="detail">
+		<div class="info">
+			<div class="icon"></div>
+			<div class="text">
+				<h2>Tor</h2>
+                <p>An unofficial redesign for the Tor site.</p>
+			</div>
+		</div>
+		<div class="stage">
+			<figure>
+				<img src="/img/portfolio/shots/tor-site-light-1.png" alt="Tor" />
+			</figure>
+			<figure>
+				<img src="/img/portfolio/shots/tor-site-light-2.png" alt="Tor" />
+			</figure>
+			<figure>
+				<img src="/img/portfolio/shots/tor-site-light-3.png" alt="Tor" />
+			</figure>
+		</div>
+	</div>
+</div>
+
+<?php include 'footer.html' ?>
